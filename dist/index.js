2c402e8a77f2808a8a3e7c2b0a420df9f78c9c1e
YurySamakhvalau <dev@takemake.com>
2022-09-08 15:26:44 +0300

    Updated version

--- dist/index.js
+++ dist/index.js
@@ -1,34 +1,32 @@
 import Swiper from "swiper";
-const swiperAdaptive = (
-  selector,
-  options,
-  breakpoint = 0,
-  direction = "down"
-) => {
-  // we can't call the Swiper if the breakpoint less 320px
-  if (breakpoint < 320) {
-    new Swiper(selector, options);
-  }
-  let instance = undefined;
-  const initSwiper = () => {
-    const documentWidth = document.documentElement.clientWidth;
-    if (direction === "down") {
-      if (documentWidth > breakpoint && instance === undefined) {
-        instance = new Swiper(selector, options);
-      } else if (documentWidth <= breakpoint && instance !== undefined) {
-        instance.destroy();
-        instance = undefined;
-      }
-    } else {
-      if (documentWidth <= breakpoint && instance === undefined) {
-        instance = new Swiper(selector, options);
-      } else if (documentWidth > breakpoint && instance !== undefined) {
-        instance.destroy();
-        instance = undefined;
-      }
+const swiperAdaptive = (selector, options, breakpoint = 0, direction = "down") => {
+    // we can't call the Swiper if the breakpoint less 320px
+    if (breakpoint < 320) {
+        new Swiper(selector, options);
     }
-  };
-  initSwiper();
-  window.addEventListener("resize", initSwiper);
+    let instance = undefined;
+    const initSwiper = () => {
+        const documentWidth = document.documentElement.clientWidth;
+        if (direction === "down") {
+            if (documentWidth > breakpoint && instance === undefined) {
+                instance = new Swiper(selector, options);
+            }
+            else if (documentWidth <= breakpoint && instance !== undefined) {
+                instance.destroy();
+                instance = undefined;
+            }
+        }
+        else {
+            if (documentWidth <= breakpoint && instance === undefined) {
+                instance = new Swiper(selector, options);
+            }
+            else if (documentWidth > breakpoint && instance !== undefined) {
+                instance.destroy();
+                instance = undefined;
+            }
+        }
+    };
+    initSwiper();
+    window.addEventListener("resize", initSwiper);
 };
 export default swiperAdaptive;
